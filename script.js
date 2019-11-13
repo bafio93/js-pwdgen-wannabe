c9fdafd89d908903b6d1c90a56d8f4c360b096d6
Previtali_Fabio <fabprev93@gmail.com>
2019-11-13 17:48:16 +0100

    Done!

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ var cognome = prompt("Inserisci il tuo cognome:");
 var colore = prompt("Inserisci il tuo colore preferito:");
 var pw = nome + cognome + colore + "19";
 document.getElementById("password").innerHTML = pw;
+/*
+How does this thing works? It looks for the element with Id "password" in the document, then it changes its value to pw.
+*/
